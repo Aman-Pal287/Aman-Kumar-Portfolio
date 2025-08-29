8cf6404e28dd4e92885a9ffb7192b859b31081a1
Aman-Pal287 <palaman9786@gmail.com>
2025-08-29 17:36:43 +0530

    agence hero section

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,10 +3,10 @@ const Video = () => {
     <>
       <video
         className="w-full h-full object-cover"
-        // autoPlay
+        autoPlay
         muted
         loop
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756400426~exp=1756404026~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=e9855b1ae970861590837ca8501d911ba068a4c8ca862e251139ec5e8908fa36&r=dXMtd2VzdDE%3D"
+        src="https://player.vimeo.com/progressive_redirect/playback/899938964/rendition/1080p/file.mp4?loc=external&log_user=0&signature=6a2cb0320d09d02dcfdfa2ad504c89a510e6d93d55e9060b1b55d680fa2e2be9"
       ></video>
     </>
   );
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
 const Agence = () => {
-  return (
-    <div>Agence</div>
-  )
-}
+  gsap.registerPlugin(ScrollTrigger);
 
-export default Agence
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
+  ];
+
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 26.7%",
+        end: "top -140%",
+        scrub: true,
+        pin: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
+
+  return (
+    <main>
+      <section className="agence-section-1">
+        <div
+          ref={imageDivRef}
+          className="absolute overflow-hidden rounded-xl top-50 left-[30vw] h-[20vw] w-[15vw] bg-red-900 "
+        >
+          <img
+            ref={imageRef}
+            className="h-full w-full object-cover"
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
+
+        <div className=" relative font-[font2] ">
+          <div className="mt-[55vh]">
+            <h1 className="text-[19vw] text-center uppercase leading-[17vw]">
+              Soixan7e
+              <br />
+              Douze
+            </h1>
+          </div>
+          <div className="pl-[40%] ">
+            <p className="text-5xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="agence-section-2"></section>
+    </main>
+  );
+};
+
+export default Agence;
